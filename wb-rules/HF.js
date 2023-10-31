82f5710de08979cf10efddc104683ee33240a0fa
Picmaste <picmast@gmail.com>
2023-10-31 19:34:53 +0300

    crate Floor_heating moduls

--- wb-rules/HF.js
+++ wb-rules/HF.js
@@ -1,3 +1,14 @@
+//////////////////////////////////////////////////////////////////////////
+//
+// Создание виртулалных драйверов для взаимодействия с Wevo
+//
+//  Модуль тёплых полов 
+//
+////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
 defineVirtualDevice("floor_heating1",  {
   
   title: "Тёплый пол на кухни",
@@ -5,8 +16,8 @@ defineVirtualDevice("floor_heating1",  {
     
       switch: {
         title: "ON/OFF",
-		type: "switch",
-		value: false
+		  type: "switch",
+		  value: false
 
         },// switch
       temp: {
@@ -16,8 +27,50 @@ defineVirtualDevice("floor_heating1",  {
         max: 32,
         min: 18
 
-        },// Dimm
+        },// range
       
         }  // calls  
 });    //      floor_heating1
+
+// Переменные 
+
+
+var _fh_condition=dev["/devices/wb-w1/controls/28-95e1ae2ecdff"];   // Текущее состояние  температуры
+var _fh_frequency=1000;   // частота в мс.
+var _fh_ON_OFF=dev["floor_heating1/switch"];  
+
+var _fh_set=dev["floor_heating1/temp"];         // Уставка 
+var _fh_delta=3;         // Delta
+var timer_id = null;
+var _fh_rele= dev["/devices/wb-mr6c-nc_109/controls/K6"];
+
+//
+
+function FH_RUN () {
+
+  log("Проверка температуры тёплого пола ");
+  if  (_fh_condition > _fh_set+_fh_delta) {
+    log("Температура выше уставки, выключаем реле");
+    _fh_rele=false;
+    }
+    (_fh_condition < _fh_set-_fh_delta) {
+      _fh_rele=true;
+      log("Температура ниже уставки, включаем реле");
+      }
+}/
+
+defineRule({
+  whenChanged: _fh_ON_OFF,
+ then: function (newValue, devName, cellName) {
+
+    if (newValue) {
+      log ("Включаем модуль тёплых полов");
+      timer_id= setInterval( FH_RUN , 1000);
+      return;
+    }
+
+    сlearTimeout(timer_id);
+    fh_rele=false;  
+ }
+ });
 
