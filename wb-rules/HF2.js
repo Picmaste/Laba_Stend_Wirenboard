04e1a6b263b8e58ed36c2a4f973a779448520dd7
Picmaste <picmast@gmail.com>
2023-11-30 16:34:34 +0000

    DEV

--- wb-rules/HF2.js
+++ wb-rules/HF2.js
@@ -37,7 +37,7 @@ defineVirtualDevice("floor_heating2",  {
   var _fh_frequency=5000;   // частота в мс.
   var _fh_ON_OFF="floor_heating2/switch";  
   
-  var _fh_set=dev["floor_heating2/temp"];         // Уставка 
+  var _fh_set="floor_heating2/temp";         // Уставка 
   var _fh_delta=1;         // Delta
   var timer_id = null;
   var _fh_rele= "wb-mr6cu_34/K3";
@@ -45,13 +45,13 @@ defineVirtualDevice("floor_heating2",  {
   //
   
   function FH_RUN1 () {
-    log("Проверка температуры тёплого пола в  {},  Температура = {}, Уставка = {} ",_fh_namber , dev[ _fh_condition ] ,_fh_set  );
-     if  (dev[ _fh_condition ] > _fh_set+_fh_delta) {
+    log("Проверка температуры тёплого пола в  {},  Температура = {}, Уставка = {} ",_fh_namber , dev[_fh_condition],dev[_fh_set]  );
+     if  (dev[ _fh_condition ] > dev[_fh_set]+_fh_delta) {
       log("Температура в {} выше уставки, выключаем реле ", _fh_namber);
       dev[_fh_rele]=false;
       return;
       }
-    if  ( dev[ _fh_condition ] < _fh_set-_fh_delta) {
+    if  ( dev[ _fh_condition ] < dev[_fh_set]-_fh_delta) {
         dev[_fh_rele]=true;
         log("Температура в {} ниже уставки, включаем реле", _fh_namber);
         }
@@ -77,7 +77,7 @@ defineVirtualDevice("floor_heating2",  {
     whenChanged: _fh_ON_OFF,
    then: function (newValue, devName, cellName) {
       if (newValue) {
-        log ("Включаем модуль тёплых полов в {} , Температура  = {}, Уставка = {}  ",_fh_namber,_fh_condition ,_fh_set );
+        log ("Включаем модуль тёплых полов в {} , Температура  = {}, Уставка = {}  ",_fh_namber,dev[_fh_condition], dev[_fh_set] );
         
         timer_id = setInterval(FH_RUN1,_fh_frequency);
         return;
